c1ebb6300b9f2a58ce3585874fdaba69fa431154
Sboniso Mjaja <sgmjaja@gmail.com>
2021-07-20 14:21:11 -0700

    current source file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef FILE
+#define FILE
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+#endif
